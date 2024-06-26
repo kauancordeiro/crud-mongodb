828c9eced4a4e2358622b71c5b9eb641748c52d2
unknown <cvitorcordeiro@gmail.com>
2024-06-26 20:43:23 -0300

    query with date and text params

--- src/main/java/com/example/demo/resources/PostResource.java
+++ src/main/java/com/example/demo/resources/PostResource.java
@@ -3,10 +3,12 @@ package com.example.demo.resources;
 import com.example.demo.domain.Post;
 import com.example.demo.resources.util.URL;
 import com.example.demo.services.PostService;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -30,5 +32,16 @@ public class PostResource {
         List<Post> posts = service.findByTitle(text);
 
         return ResponseEntity.ok().body(posts);
+    }
+    @GetMapping("/fullsearch")
+    public ResponseEntity<List<Post>> fullSearch(@RequestParam(value = "text",defaultValue = "") String text,
+                                                 @RequestParam(value = "minDate",defaultValue = "") String minDate,
+                                                 @RequestParam(value = "maxDate", defaultValue = "") String maxDate){
+        text = URL.decodeParam(text);
+       Date min = URL.convertDate(minDate, new Date(0L));
+       Date max = URL.convertDate(maxDate, new Date());
+       List<Post> posts = service.fullSearch(text,min,max);
+
+       return ResponseEntity.ok().body(posts);
     }
 }
--- src/main/java/com/example/demo/services/PostService.java
+++ src/main/java/com/example/demo/services/PostService.java
@@ -9,6 +9,7 @@ import com.example.demo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,6 +28,12 @@ public class PostService {
 
     public List<Post> findByTitle(String title){
         return repository.findByTitle(title);
+    }
+
+    public List<Post> fullSearch(String text, Date minDate,Date maxDate){
+        maxDate = new Date(maxDate.getTime() + 24* 60 * 60 * 1000);
+        return repository.fullSearch(text,minDate,maxDate);
+
     }
 
 }
