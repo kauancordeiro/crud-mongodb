3cfdd569e2491ffd319c861370a2a8f42a89f47f
unknown <cvitorcordeiro@gmail.com>
2024-06-25 22:31:24 -0300

    entity post

--- src/main/java/com/example/demo/config/Instantiation.java
+++ src/main/java/com/example/demo/config/Instantiation.java
@@ -1,29 +1,51 @@
 package com.example.demo.config;
 
+import com.example.demo.domain.Post;
 import com.example.demo.domain.User;
+import com.example.demo.repositories.PostRepository;
 import com.example.demo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
+
 @Configuration
 public class Instantiation implements CommandLineRunner {
 
     @Autowired
-    private UserRepository repository;
+    private UserRepository userRepository;
+
+    @Autowired
+    private PostRepository postRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
-        repository.deleteAll();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+        userRepository.deleteAll();
+        postRepository.deleteAll();
 
         User maria = new User(null, "Maria Brown","maria@gmail.com" );
         User alex = new User(null, "Alex Green","alex@gmail.com" );
         User bob = new User(null, "Bob Grey","bob@gmail.com" );
 
-        repository.save(maria);
-        repository.save(alex);
-        repository.save(bob);
+        userRepository.save(maria);
+        userRepository.save(alex);
+        userRepository.save(bob);
+
+        Post post1 = new Post(null,sdf.parse("21/03/2018"),"Partiu Viagem", "Vou viajar para SP. Abraços.", maria);
+        Post post2 = new Post(null,sdf.parse("23/03/2018"),"Bom dia", "Acordei feliz hoje!",alex);
+        Post post3 = new Post(null,sdf.parse("25/03/2018"),"Partiu mandela", "Hoje é aonde?",bob);
+
+        postRepository.save(post1);
+        postRepository.save(post2);
+        postRepository.save(post3);
+
 
     }
 }
